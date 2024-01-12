a5040b11462ac63b763637488cc8edd32f1bc775
Lookman <daniel.lookman@mediq.com>
2024-01-12 15:24:39 +0100

    Added Snippets folder and Convert-SecureString-to-PlainText.ps1

---
+++ Snippets/Convert-SecureString-to-PlainText.ps1
@@ -0,0 +1,3 @@
+﻿$string = "XXACBBB12345"
+$secureString = $string | ConvertTo-SecureString -Force -Asplaintext
+(New-Object System.Management.Automation.PSCredential('username', $secureString)).GetNetworkCredential().Password
