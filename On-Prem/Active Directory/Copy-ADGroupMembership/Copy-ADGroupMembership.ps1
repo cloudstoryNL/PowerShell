a81ea79fc1ce990c9dc2835bbe685d0c41fd9861
Lookman <daniel.lookman@mediq.com>
2024-04-16 10:31:38 +0200

    Added Get-AuthenticationToken.ps1

---
+++ On-Prem/Active Directory/Copy-ADGroupMembership/Copy-ADGroupMembership.ps1
@@ -0,0 +1,69 @@
+﻿<#
+.Synopsis
+   This function enables you in easily copying groups from an Active Directory User to another Active Directory User
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Copy-ADGroupMembership
+{
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $sourceAccount,
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        $destinationAccount,
+        $domainController,
+        $secureCredential,
+        $excludedGroups
+    )
+
+    if(($null -eq $domainController) -and ($null -eq $secureCredential))
+    {
+        Get-ADUser -Identity $sourceAccount | Select-Object -ExpandProperty MemberOf | Get-ADGroup | ForEach-Object {
+            if(!($excludedGroups.Contains($_.Name)))
+            {
+                Add-ADGroupMember -Identity $_.SID.Value -Members $destinationAccount
+            }
+        }
+    }
+
+    if(($null -eq $domainController) -and ($null -ne $secureCredential))
+    {
+        Get-ADUser -Identity $sourceAccount -Credential $secureCredential | Select-Object -ExpandProperty MemberOf | Get-ADGroup -Credential $secureCredential | ForEach-Object {
+            if(!($excludedGroups.Contains($_.Name)))
+            {
+                Add-ADGroupMember -Identity $_.SID.Value -Members $destinationAccount -Credential $secureCredential
+            }
+        }
+    }
+
+    if(($null -ne $domainController) -and ($null -eq $secureCredential))
+    {
+        Get-ADUser -Identity $sourceAccount -Server $domainController | Select-Object -ExpandProperty MemberOf | Get-ADGroup -Server $domainController | ForEach-Object {
+            if(!($excludedGroups.Contains($_.Name)))
+            {
+                Add-ADGroupMember -Identity $_.SID.Value -Members $destinationAccount -Server $domainController
+            }
+        }
+    }
+
+    if(($null -ne $domainController) -and ($null -ne $secureCredential))
+    {
+        Get-ADUser -Identity $sourceAccount -Server $domainController -Credential $secureCredential | Select-Object -ExpandProperty MemberOf | Get-ADGroup -Server $domainController -Credential $secureCredential | ForEach-Object {
+            if(!($excludedGroups.Contains($_.Name)))
+            {
+                Add-ADGroupMember -Identity $_.SID.Value -Members $destinationAccount -Server $domainController -Credential $secureCredential
+            }
+        }
+    }
+    
+}
+
---
+++ Snippets/Get-AuthenticationToken.ps1
@@ -0,0 +1,28 @@
+﻿function Get-AuthenticationToken
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)] $tenantID,
+        [Parameter(Mandatory=$true)] $appID,
+        [Parameter(Mandatory=$true)] $appSecret,
+        [Parameter(Mandatory=$true)] $apiUri)
+
+        $OAuthUri = "https://login.microsoftonline.com/$tenantID/oauth2/token"
+
+        $authBody = [Ordered] @{
+            resource = "$apiUri"
+            client_id = "$appID"
+            client_secret = "$appSecret"
+            grant_type = 'client_credentials'
+        }
+
+        Try{
+            $authResponse = Invoke-RestMethod -Method Post -Uri $OAuthUri -Body $authBody -ErrorAction Stop
+            $authToken    = $authResponse.access_token
+
+            return $authToken
+        } Catch {
+            Write-Host $error[0].Exception
+        }        
+}
