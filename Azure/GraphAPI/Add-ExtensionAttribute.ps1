2201df2332038ceeea1386e99d49de259d50b1ce
cloudstoryNL <123375984+cloudstoryNL@users.noreply.github.com>
2025-04-23 13:16:26 +0200

    Add Add-ExtensionAttribute.ps1
    Add an extensionAttribute to an existing entra-id only account.

---
+++ Azure/GraphAPI/Add-ExtensionAttribute.ps1
@@ -0,0 +1,7 @@
+﻿$params = @{
+  onPremisesExtensionAttributes = @{
+    extensionAttribute14 = "corpnet.one-id.it/CORPNET/Germany/"
+  }
+}
+
+Update-MgUser -UserId 'user@domain.com' -BodyParameter $params
